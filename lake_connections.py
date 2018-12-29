46628894bc829aba3bb431796c6aafaf114194bc
jlthornton <46246014+jlthornton@users.noreply.github.com>
2018-12-29 11:37:03 -0500

    Create lake_connections.py
    Initial setup

---
+++ lake_connections.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+A description of the problem we're solving and
+background info on the data set.
+"""
+
+__author__ = "Jessica Weir"
+__copyright__ = "Copyright 2019"
+__credits__ = ["Jessica Weir", "Hillary Jeffrey"]
+__license__ = "GPL"
+__version__ = "1.0"
+__status__ = "Development"
+
+
+# Standard library imports
+from arcgis.gis import GIS
+
+# Connect to GIS
+# user = 'arcgis_python'
+# password = 'default'
+# my_gis = GIS('https://www.arcgis.com', user_name, password)
+
